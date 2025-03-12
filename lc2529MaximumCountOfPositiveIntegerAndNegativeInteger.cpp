386bbab9f63cb36e5257d6ac5764ca3cda4f89d9
CosmicIon <harshnivedan@gmail.com>
2025-03-12 15:07:50 +0530

    added binary search code for leetcode 2529

--- lc2529MaximumCountOfPositiveIntegerAndNegativeInteger.cpp
+++ lc2529MaximumCountOfPositiveIntegerAndNegativeInteger.cpp
@@ -5,6 +5,10 @@
 // In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
 // Note that 0 is neither positive nor negative.
 
+
+
+
+// linear search.
 class Solution {
     public:
         int maximumCount(vector<int>& nums) {
@@ -17,5 +21,47 @@ class Solution {
                 }
             }
             return max(p, n);
+        }
+    };
+
+
+
+
+
+//binary search
+class Solution {
+    public:
+        int maximumCount(vector<int>& nums) {
+            int s = 0;
+            int e = nums.size() - 1;
+            int f = -1, l = -1;
+    
+            while (s <= e) {
+                int mid = s + (e - s) / 2;
+                if (nums[mid] < 0) {
+                    f = mid;
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
+            
+            s = 0;
+            e = nums.size() - 1;
+            while (s <= e) {
+                int mid = s + (e - s) / 2;
+                if (nums[mid] > 0) {
+                    l = mid;
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            }
+    
+            e = nums.size();
+            if (f == -1 && l == -1) return 0;
+            else if (f == -1) return e - l;
+            else if (l == -1) return f + 1;
+            else return max(f + 1, e - l);
         }
     };
