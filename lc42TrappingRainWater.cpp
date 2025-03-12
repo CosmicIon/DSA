b2c19e20b7b0bfcb1ff3894489a192ac436eece9
CosmicIon <harshnivedan@gmail.com>
2025-03-12 19:52:10 +0530

    added optimized code in leetcode 42(trapping rain water)

--- lc42TrappingRainWater.cpp
+++ lc42TrappingRainWater.cpp
@@ -51,3 +51,37 @@ class Solution {
             return water;
         }
     };
+
+
+
+
+//Optimized code
+class Solution {
+    public:
+        int trap(vector<int>& height) {
+            if (height.empty()) return 0;
+    
+            int left = 0, right = height.size() - 1;
+            int leftMax = 0, rightMax = 0;
+            int water = 0;
+    
+            while (left < right) {
+                if (height[left] < height[right]) {
+                    if (height[left] >= leftMax) {
+                        leftMax = height[left];
+                    } else {
+                        water += leftMax - height[left];
+                    }
+                    left++;
+                } else {
+                    if (height[right] >= rightMax) {
+                        rightMax = height[right];
+                    } else {
+                        water += rightMax - height[right];
+                    }
+                    right--;
+                }
+            }
+            return water;
+        }
+    };
