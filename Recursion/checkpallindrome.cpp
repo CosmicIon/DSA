205550829c75d0300f714292f2d892fdde01322d
CosmicIon <harshnivedan@gmail.com>
2025-08-19 23:29:23 +0530

    check pallilndrome with one variable

--- Recursion/checkpallindrome.cpp
+++ Recursion/checkpallindrome.cpp
@@ -1,20 +1,36 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-bool checkPallindrom(string &s, int i, int j) {
-    //base case
-    if(i>j) 
+bool checkPallindrom(string &s, int i, int j)
+{
+    // base case
+    if (i > j)
         return true;
-    if(s[i] != s[j]) 
+    if (s[i] != s[j])
         return false;
 
-    checkPallindrom(s, i+1, j-1); //recursive call
+    checkPallindrom(s, i + 1, j - 1); // recursive call
 }
 
-int main() {
+// with only one variable
+bool checkPallindromWithOneVariable(string &s, int i)
+{
+    // base case
+    if (i > s.length() - 1 - i)
+        return true;
+    if (s[i] != s[s.length() - 1 - i])
+        return false;
+
+    checkPallindromWithOneVariable(s, i + 1); // recursive call
+}
+
+int main()
+{
     string s = "abbcbba";
-    cout << checkPallindrom(s, 0, s.length()-1) << endl;
+    cout << checkPallindrom(s, 0, s.length() - 1) << endl;
+
+    cout << checkPallindromWithOneVariable(s, 0) << endl;
 
     return 0;
 }
