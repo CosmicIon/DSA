2b2506431dd6810a86aea8b2100b7e6522614788
CosmicIon <harshnivedan@gmail.com>
2026-08-21 22:26:58 +0530

    leetcode 225

--- Queue/Gas-Station.cpp
+++ Queue/Gas-Station.cpp
@@ -42,3 +42,27 @@ class Solution {
         else return -1;
     }
 };
+
+
+
+class Solution {
+public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        int start = 0;
+        int balance = 0;
+        int total = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            int diff = gas[i] - cost[i];
+            balance += diff;
+            total += diff;
+
+            if (balance < 0) {
+                start = i + 1;
+                balance = 0;
+            }
+        }
+
+        return (total >= 0) ? start : -1;
+    }
+};
